dac2f32c0046a47156d4625c307e66089319bde3
Shazan Zaidi <rit609009@gmail.com>
2025-10-22 11:01:17 +0530

    Initial Commit

---
+++ Budgety/BudgetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetyApp.swift
+//  Budgety
+//
+//  Created by Shazan Zaidi on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
